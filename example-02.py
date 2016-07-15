2e2f15f82f504fb43e66c19c2a23fcbf2fe86042
Ashton Baker <ashtonsb@rhus.eeb.lsa.umich.edu>
2016-07-15 17:14:16 -0400

    fixed file directory issue in example 3

--- example-02.py
+++ example-02.py
@@ -3,7 +3,7 @@
 # between two pieces, because it give explicit note numbers
 
 import music
-pattern_1 = music.midi_import('bwv529-1.mid')
-pattern_2 = music.midi_import('bwv0541f.mid')
+pattern_1 = music.midi_import('./midi/bwv529-1.mid')
+pattern_2 = music.midi_import('./midi/bwv0541f.mid')
 x = music.comparison_matrix(pattern_1, pattern_2)
 music.summarize_similarity(x, 10)
